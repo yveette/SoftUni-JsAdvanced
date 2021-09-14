8406887e27afd874800ccf5464c578207792424e
yveette <dimitrova.v.ivet@gmail.com>
2021-09-14 22:26:02 +0300

    Added task 1

---
+++ 01-SyntaxFunctionsAndStatements-Lab/01-EchoFunction.js
@@ -0,0 +1,12 @@
+function echo(word){
+    console.log(word.length);
+    console.log(word);
+}
+
+echo('Hello, JavaScript!');
+// 18
+// Hello, JavaScript!
+
+echo('strings are easy');
+// 16
+// Hello, JavaScript!
