de06546d8fb3af3941b3008ca63b3ac2b0a97abb
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-09-17 13:58:20 +0300

    Added 07-DaysOfWeek

---
+++ 01-SyntaxFunctionsAndStatements-Lab/07-DayOfWeek.js
@@ -0,0 +1,34 @@
+function weekDays(string) {
+    let result;
+    switch (string) {
+        case "Monday":
+            result = 1;
+            break;
+        case "Tuesday":
+            result = 2;
+            break;
+        case "Wednesday":
+            result = 3;
+            break;
+        case "Thursday":
+            result = 4;
+            break;
+        case "Friday":
+            result = 5;
+            break;
+        case "Saturday":
+            result = 6;
+            break;
+        case "Sunday":
+            result = 7;
+            break;
+        default:
+            result = "error";
+            break;
+    }
+    console.log(result);
+}
+
+weekDays('Monday'); // 1
+weekDays('Friday'); // 5
+weekDays('Invalid'); // error
