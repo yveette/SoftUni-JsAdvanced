f4deeca70d268d22e98acd196d30359b23751a2c
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-09-21 14:08:36 +0300

    08-SortAnArrayBy2Criteria

---
+++ 04-ArraysAndNestedArrays-Exercise/08-SortAnArrayBy2Criteria.js
@@ -0,0 +1,45 @@
+function sortBy2(arr) {
+    let sorted = arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
+
+    for (let el of sorted) {
+        console.log(el);
+    }
+}
+
+sortBy2(['alpha',
+    'beta',
+    'gamma']
+);
+/*
+beta
+alpha
+gamma
+*/
+console.log('----------');
+
+sortBy2(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']
+);
+/*
+Jack
+Isacc
+George
+Theodor
+Harrison
+*/
+
+console.log('----------');
+
+sortBy2(['test',
+    'Deny',
+    'omen',
+    'Default']
+);
+/*Deny
+omen
+test
+Default
+*/
