48187c5fff95df6c616342fff9909240a1baa54b
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-09-21 13:21:34 +0300

    04-RotateArray

---
+++ 04-ArraysAndNestedArrays-Exercise/04-RotateArray.js
@@ -0,0 +1,24 @@
+function rotate(arr, n) {
+    // if rotations are many ---> n % arr.length
+    for (let i = 0; i < n; i++) {
+        arr.unshift(arr.pop())
+    }
+
+    console.log(arr.join(' '));
+}
+
+rotate(['1',
+    '2',
+    '3',
+    '4'],
+    2
+);
+// 3 4 1 2
+
+rotate(['Banana',
+    'Orange',
+    'Coconut',
+    'Apple'],
+    15
+);
+// Orange Coconut Apple Banana
