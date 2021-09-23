8c7ea343bb031433d5591381f334a06ecc9b6c67
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-09-23 19:55:38 +0300

    Added 04-ObjectFactory.js

---
+++ 05-ObjectsAndComposition-Lab/04-ObjectFactory.js
@@ -0,0 +1,74 @@
+function factory(library, orders) {
+  return orders.map(compose);
+
+  function compose(order) {
+    // Create empty object and copy properties from template
+    const result = Object.assign({}, order.template);
+
+    // Compose methods from library for every item in parts
+    for (let part of order.parts) {
+      result[part] = library[part];
+    }
+
+    // Return result
+    return result;
+  }
+}
+
+
+
+const library = {
+  print: function () {
+    console.log(`${this.name} is printing a page`);
+  },
+  scan: function () {
+    console.log(`${this.name} is scanning a document`);
+  },
+  play: function (artist, track) {
+    console.log(`${this.name} is playing '${track}' by ${artist}`);
+  },
+};
+
+const orders = [
+  {
+    template: { name: 'ACME Printer' },
+    parts: ['print']
+  },
+  {
+    template: { name: 'Initech Scanner' },
+    parts: ['scan']
+  },
+  {
+    template: { name: 'ComTron Copier' },
+    parts: ['scan', 'print']
+  },
+  {
+    template: { name: 'BoomBox Stereo' },
+    parts: ['play']
+  }
+];
+const products = factory(library, orders);
+console.log(products);
+
+//output:
+/*
+[
+{
+  name: 'ACME Printer',
+  print: [Function: print]
+},
+{
+  name: 'Initech Scanner',
+  scan: [Function: scan]
+},
+{
+  name: 'ComTron Copier',
+  scan: [Function: scan],
+  print: [Function: print]
+},
+{
+  name: 'BoomBox Stereo',
+  play: [Function: play]
+}
+]
+*/
