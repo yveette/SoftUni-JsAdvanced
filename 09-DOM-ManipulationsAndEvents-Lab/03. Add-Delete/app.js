412cb7cbd982786e3c7c4ec69fb241fa19483e57
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-10-04 22:50:39 +0300

    Added 03.Add-Delete

---
+++ 09-DOM-ManipulationsAndEvents-Lab/03. Add-Delete/app.js
@@ -0,0 +1,27 @@
+function addItem() {
+    // select input field and read value
+    const input = document.getElementById('newItemText');
+
+    // create new <li> element and set its content to input value
+    const liElement = document.createElement('li');
+    liElement.textContent = input.value;
+
+    // create Delete button
+    const button = document.createElement('a');
+    button.href = '#';
+    button.textContent = '[Delete]';
+    button.addEventListener('click', removeElement);
+
+    liElement.appendChild(button);
+
+    // select <ul> and append new <li> element
+    document.getElementById('items').appendChild(liElement)
+
+    // nice to have: clear input field
+    input.value = '';
+
+    function removeElement(ev) {
+        const parent = ev.target.parentNode;
+        parent.remove();
+    }
+}
