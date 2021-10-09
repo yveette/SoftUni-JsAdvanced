ea15ce3e03f8665ce308f81b9b02a2ea208281c9
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-10-09 18:37:13 +0300

    Added 02.Add.js

---
+++ 11-Advanced Functions-Lab/02.Add.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    let num = n;
+
+    return function add5(n) {
+        return num + n;
+    },
+    function add7(n) {
+        return num + n;
+    }
+}
+
+
+
+let add5 = solution(5);
+console.log(add5(2)); // 7
+console.log(add5(3)); // 8
+
+
+
+let add7 = solution(7);
+console.log(add7(2)); // 9
+console.log(add7(3)); // 10
