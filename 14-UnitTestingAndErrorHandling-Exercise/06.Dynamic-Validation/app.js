1a2594ee30dd444be15943e210bb654ef828f9df
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-10-13 19:41:34 +0300

    Fill app.js with solution

--- 14-UnitTestingAndErrorHandling-Exercise/06.Dynamic-Validation/app.js
+++ 14-UnitTestingAndErrorHandling-Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    const inputField = document.getElementById('email');
+    const validMailPattern = /(^[a-z]+@[a-z+\.[a-z]+$)/;
+
+    inputField.addEventListener('change', () => {
+        if (!validMailPattern.test(inputField.value)) {
+            inputField.classList.add('error');
+        } else {
+            inputField.classList.remove('error');
+        }
+    })
 }
