5282431f32510d8f12a81557ad67998255bc9150
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-10-13 19:17:31 +0300

    Added 01.RequestValidator.js

---
+++ 14-UnitTestingAndErrorHandling-Exercise/01.RequestValidator.js
@@ -0,0 +1,59 @@
+function validator(obj) {
+    const validMethods = ["GET", "POST", "DELETE", "CONNECT"];
+    const validVersions = ["HTTP/0.9", "HTTP/1.0", "HTTP/1.1", "HTTP/2.0"];
+    const uriRegex = /(^[\w.]+$)/;
+    const messageRegex = /([<>\\&'"])/;
+
+    if (!obj.method || !validMethods.includes(obj.method)) {
+        throw new Error(`Invalid request header: Invalid Method`);
+    }
+
+    if (!obj.uri || obj.uri == "" || !uriRegex.test(obj.uri)) {
+        throw new Error(`Invalid request header: Invalid URI`);
+    }
+
+    if (!obj.version || !validVersions.includes(obj.version)) {
+        throw new Error(`Invalid request header: Invalid Version`);
+    }
+
+    if (obj.message == undefined || messageRegex.test(obj.message)) {
+        throw new Error(`Invalid request header: Invalid Message`);
+    }
+
+    return obj;
+}
+
+console.log(validator({
+    method: 'GET',
+    uri: 'svn.public.catalog',
+    version: 'HTTP/1.1',
+    message: ''
+}));
+
+/*
+{
+  method: 'GET',
+  uri: 'svn.public.catalog',
+  version: 'HTTP/1.1',
+  message: ''
+}
+*/
+
+
+
+console.log(validator({
+    method: 'OPTIONS',
+    uri: 'git.master',
+    version: 'HTTP/1.1',
+    message: '-recursive'
+}));
+//   Invalid request header: Invalid Method
+
+
+
+console.log(validator({
+    method: 'POST',
+    uri: 'home.bash',
+    message: 'rm -rf /*'
+}));
+//   Invalid request header: Invalid Version
